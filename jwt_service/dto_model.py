5f92ceeb083a6df0020a9b078688cd62c8d45a6a
afenk <artyom.antnv@gmail.com>
2023-10-10 21:34:05 +0300

    update_pydantic

--- jwt_service/dto_model.py
+++ jwt_service/dto_model.py
@@ -4,6 +4,5 @@ from humps import camelize
 
 class DTOModel(BaseModel):
     class Config:
-        orm_mode = True
-        allow_population_by_field_name = True
+        populate_by_name = True
         alias_generator = camelize

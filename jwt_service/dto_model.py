65713eb08149798a6f71d91b12c4215c0c400463
afenk <artyom.antnv@gmail.com>
2023-10-30 19:20:01 +0200

    update_pydantic

--- jwt_service/dto_model.py
+++ jwt_service/dto_model.py
@@ -1,8 +1,10 @@
-from pydantic import BaseModel
-from humps import camelize
+from pydantic import BaseModel, ConfigDict
+from pydantic.alias_generators import to_camel
 
 
 class DTOModel(BaseModel):
-    class Config:
-        populate_by_name = True
-        alias_generator = camelize
+    model_config = ConfigDict(
+        extra='forbid',
+        populate_by_name=True,
+        alias_generator=to_camel,
+    )
